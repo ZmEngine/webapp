0c32fcff766b89e900b90fcb137876465a4e735d
张猛 <496472571@qq.com>
2018-08-05 21:56:36 +0800

    完成音乐播放器,图片进入效果,问题,回答

--- VueTest/vuetest/src/App.vue
+++ VueTest/vuetest/src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <div :class="music_btn" id="audioBtn" @click="backyinyaoClick" ></div>
+    <audio src="../static/sucai/backyinyao.mp3" id="musicPlay" autoplay="" loop=""></audio>
     <transition name="router-fade" mode="out-in">
       <keep-alive>
         <router-view v-if="$route.meta.keepAlive"></router-view>
@@ -13,7 +15,24 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      music_btn: 'audioon'
+    }
+  },
+  methods: {
+    backyinyaoClick: function () {
+      let audio = document.querySelector('#musicPlay')
+      if (this.music_btn === 'audioon') {
+        this.music_btn = 'audioff'
+        audio.pause()
+      } else {
+        this.music_btn = 'audioon'
+        audio.play()
+      }
+    }
+  }
 }
 
 </script>
@@ -27,5 +46,28 @@ export default {
     color: #2c3e50;
     margin-top: 60px;
   }
-
+#audioBtn {
+  position: absolute;
+  background-size: 100%;
+  width: 0.6rem;
+  height: 0.6rem;
+  top: 1%;
+  right: 1%;
+  z-index: 99;
+  animation: xuanzhuan 1s infinite linear;
+}
+.audioon{
+  background: url("../static/sucai/on.png") no-repeat;
+}
+.audioff{
+  background: url("../static/sucai/off.png") no-repeat;
+}
+@keyframes xuanzhuan {
+ from{
+    transform: rotate(0deg);
+ }
+ to{
+    transform: rotate(360deg);
+  }
+}
 </style>
--- VueTest/vuetest/src/pages/minitTest.vue
+++ VueTest/vuetest/src/pages/minitTest.vue
@@ -1,23 +1,80 @@
 <template>
-    <div>
-      <mt-button type="default" @click.native="handleClick" >default</mt-button>
-      <img src='../../static/img/logo.png' />
+    <div class='wenjuan' >
+      <div style="height:10rem;width:100%;display:flex; flex-direction: row;justify-content:center;">
+        <transition name="showimg">
+            <img v-show="loadimg" class="mainimg showimg-enter-active" :src="'../../static/img/'+imgsrc" />
+        </transition>
+      </div>
+    <div  style="height:2rem;display:flex; flex-direction: row; align-items:center;">
+      <span>问题:谁是最帅的人?</span>
+    </div>
+    <div class="xuxian"></div>
+    <div style="display:flex; flex-direction: row;justify-content:center;flex-wrap:wrap;" >
+      <mt-button type="primary" class="xuanxiang" @click.native="handleClick" ><div class="xuanxiangText">选项A:猛哥</div></mt-button>
+ <mt-button type="primary" class="xuanxiang" @click.native="handleClick" ><div class="xuanxiangText">选项B:大猛哥</div></mt-button>
+  <mt-button type="primary" class="xuanxiang" @click.native="handleClick" ><div class="xuanxiangText">选项C:张猛</div></mt-button>
+    </div>
+      <div class="xuxian" style="margin-top: 1rem;" ></div>
     </div>
 </template>
 
 <script>
-import { MessageBox } from 'mint-ui'
+// import { MessageBox } from 'mint-ui'
 export default {
   name: 'minitTest',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      loadimg: true,
+      imgsrc: 'sjw.png'
     }
   },
   methods: {
     handleClick: function () {
-      MessageBox('提示', '操作成功')
+      this.loadimg = !this.loadimg
+      this.imgsrc = 'sjw.png'
+      let vm = this
+      setTimeout(function () { vm.loadimg = true }, 1000)
+      // MessageBox('提示', '操作成功')
     }
   }
 }
 </script>
+
+<style>
+.mainimg{
+  margin: 0px auto;
+  height:10rem;
+}
+.xuxian{
+  border-bottom: blue 3px dashed;
+}
+.wenjuan{
+  font-size:0.7rem;
+  font-weight: bold;
+  color:blue;
+}
+.xuanxiang{
+  margin-top: 0.1rem;
+  color: #fff;
+  width:98%;
+}
+.xuanxiangText{
+  text-align: left;
+}
+.showimg-enter-active {
+  animation: bounce-in 1s ease-out;
+}
+.showimg-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform-origin: top left;
+    transform: rotate(-180deg);
+  }
+  100% {
+    transform-origin: top left;
+    transform: rotate(3deg);
+  }
+}
+</style>
